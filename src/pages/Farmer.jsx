d33f919a74d57f22fb96c78541d978d46393f6e8
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2025-01-17 19:31:05 +0530

    change

--- src/pages/Farmer.jsx
+++ src/pages/Farmer.jsx
@@ -60,6 +60,7 @@ const a = async (event) => {
     return;
   }
 
+  
   try {
     const factoryAddress = "0xFAae2E0bE213629F62D352CFa2cbF1fa7Eab268e"; // Replace with actual address
     // 0x2A6E75985EF4280fC823dcdb9cE7946Cb5A67e9c
